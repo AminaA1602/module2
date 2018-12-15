722aa31ef45eb6dac3e9be2017eccc26273d761e
Amina Aweis <AminaAweis1150@gmail.com>
2018-12-15 16:09:48 +0000

    project exercise for assessment

---
+++ ch13/MovingShapes.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 14 16:55:47 2018
+
+@author: Skitt
+"""
+
+from shape import*
---
+++ ch13/ch13_amina_test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 14 16:16:14 2018
+
+@author: Skitt
+"""
+
+from MovingShapes import *
