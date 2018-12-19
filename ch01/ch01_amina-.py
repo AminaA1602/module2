84b1667ed43bccb32718b5f234e7978a62672d7e
Amina Aweis <AminaAweis1150@gmail.com>
2018-12-19 09:57:21 +0000

    updated file

--- ch01/ch01_amina-.py
+++ ch01/ch01_amina-.py
@@ -29,43 +29,13 @@ print (B)
 
 # % is called modulo - it shows the remainder of an expression e.g 5%2 is 1 because 1 is the remainder #
 
-
-# ----------------- How to use variables ----------------- # 
-
-x = 12.5
-y = 3 * x + 2 
-print(y)
-
-age = 5 #---> this is an integer 
-age = "almost three" #--- > this is a string
-a_longer_name = "hello, CFG!"
-
-print(age)
-print(a_longer_name)
-
-# ----------------- Basic string manipulation ----------------- # 
-
-print ("Bob " * 3)
-print ("Bob " + 3)
-print("hello".upper()) #-----> this is called a method
-print("GOODBYE".lower()) 
-print("the lord of the rings".title()) 
-#there will be an error for print ("Bob" + 3) because you cannot have a string with an integer. 
-
-# ----------------- String formatting ----------------- #
-
-#What is string formatting? - It is a way of taking one or more variable and putting them inside a string b having placeholders for the values. 
-
-age =5 
-like = "painting"
-
-age_description ="my age is {} and I like {}.".format(age,like)
-print(age_description)
-
-# %s{} = string , %d = decimal , %r = repr (another way of returning a string and detailed information of an object's content)
-
-
-""" For the Christmas holdidays, redo the homework and keep practicing them so you don't forget """
+print ("Hello World!")
+print ("Hello Again")
+print ("I like typing this.")
+print ("This is fun.")
+print ('Yay! Printing.')
+print ("I'd much rather you 'not'.")
+print ('I "said" do not touch this.')
 
 
 
@@ -82,14 +52,4 @@ print(age_description)
 
 
 
-
-#   
-#A = 2+3
-#B = 2.9+3.2
-#C = (3.2/2.0) + 7   
-#D = 2**3
-#E = 4**(1/2)
-#F = str(6) + 'a'
-#print (A)   
-#print (B)
     
